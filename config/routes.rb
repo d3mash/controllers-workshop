afab47a30b476a75784dce01c3fe7c8b0b8268f9
Demid Kashirin <demid.kashirin@gmail.com>
2020-05-23 17:38:52 +0300

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,18 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   scope module: :web do
-    root 'home#index'
-
-    resources :users do
-
-    end
+    root 'notes#index'
 
     resources :notes do
-
+      scope module: :notes do
+        resources :comments
+      end
     end
+
+    scope module: :my do
+      resources :notes, only: :index
+    end
+
+    resources :users, only: :show
   end
 end
